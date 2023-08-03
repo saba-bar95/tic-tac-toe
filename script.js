6d20989b5009e7d38ae6b85462234d806c16d9cf
saba-bar95 <s.barbakadze95@gmail.com>
2023-08-03 18:33:10 +0400

    update a bit of code

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 const gameboardCont = document.querySelector(".gameboard-container");
 const controllerDiv = document.querySelector(".gameboard-controller");
-let chosenBtns;
+let selectBtns;
+let checkedGameboardArr = [];
 
 const Controller = (() => {
   const signBtns = document.querySelectorAll(".sign-btn");
@@ -12,37 +13,42 @@ const Controller = (() => {
 
   signBtns.forEach((el) => {
     el.addEventListener("click", function () {
-      el.classList.toggle("chosen");
+      el.classList.toggle("selected");
+
       if (
         el.nextElementSibling &&
-        el.nextElementSibling.classList.contains("chosen")
+        el.nextElementSibling.classList.contains("selected")
       )
-        el.nextElementSibling.classList.remove("chosen");
+        el.nextElementSibling.classList.remove("selected");
+
       if (
         el.previousElementSibling &&
-        el.previousElementSibling.classList.contains("chosen")
+        el.previousElementSibling.classList.contains("selected")
       )
-        el.previousElementSibling.classList.remove("chosen");
+        el.previousElementSibling.classList.remove("selected");
     });
   });
 
   startBtn.addEventListener("click", function () {
-    chosenBtns = document.querySelectorAll(".chosen");
-    if (chosenBtns.length <= 1) {
-      alert("Choose sign for both players ");
+    selectBtns = document.querySelectorAll(".selected");
+
+    if (selectBtns.length <= 1) {
+      alert("Select sign for both players ");
       return;
     }
+
     if (
-      chosenBtns.length > 1 &&
-      chosenBtns[0].textContent === chosenBtns[1].textContent
+      selectBtns.length > 1 &&
+      selectBtns[0].textContent === selectBtns[1].textContent
     ) {
-      alert("Choose different signs");
+      alert("Select different signs");
       return;
     }
+
     controllerDiv.classList.add("hidden");
     gameboardCont.classList.remove("hidden");
-    player1.sign = chosenBtns[0].textContent;
-    player2.sign = chosenBtns[1].textContent;
+    player1.sign = selectBtns[0].textContent;
+    player2.sign = selectBtns[1].textContent;
     displayContent();
   });
 
@@ -53,10 +59,9 @@ const Gameboard = (() => {
   const gameboardGrid = document.querySelector(".gameboard-grid");
   const gameboardArr = [];
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 0; i < 9; i++) {
     const gameboardItem = document.createElement("div");
     gameboardItem.classList.add("gameboard-item");
-    gameboardItem.classList.add(`item-${i}`);
     gameboardGrid.append(gameboardItem);
     gameboardArr.push(gameboardItem);
   }
@@ -66,16 +71,21 @@ const Gameboard = (() => {
 
 const displayContent = () => {
   const turnBtn = document.querySelector(".turn-btn");
-  const player1Div = document.querySelector(".player-1");
-  const player2Div = document.querySelector(".player-2");
   const player1Sign = document.createElement("p");
   const player2Sign = document.createElement("p");
+  const player1Div = document.querySelector(".player-1");
+  const player2Div = document.querySelector(".player-2");
+  const playerPara1 = document.querySelector(".player-para-1");
+  const playerPara2 = document.querySelector(".player-para-2");
+
   player1Sign.classList.add("player-sign");
   player2Sign.classList.add("player-sign");
   player1Sign.textContent = Controller.player1.sign;
   player2Sign.textContent = Controller.player2.sign;
   player1Div.appendChild(player1Sign);
   player2Div.appendChild(player2Sign);
+  playerPara1.textContent = "player 1";
+  playerPara2.textContent = "player 2";
 
   if (Controller.player1.sign === "X") {
     Controller.player1.status = "active";
@@ -88,10 +98,20 @@ const displayContent = () => {
   Gameboard.gameboardArr.forEach((el) => {
     el.addEventListener("click", function () {
       if (el.textContent === "X" || el.textContent === "O") return;
+
       if (Controller.player1.status) {
         el.textContent = Controller.player1.sign;
         Controller.player1.status = false;
+        checkedGameboardArr.push(el);
+
         if (checkResult(Gameboard)) {
+          restartGame();
+          return;
+        } else if (
+          Gameboard.gameboardArr.length === checkedGameboardArr.length
+        ) {
+          turnBtn.style.justifySelf = "center";
+          turnBtn.textContent = "It's a tie";
           restartGame();
           return;
         } else {
@@ -100,10 +120,19 @@ const displayContent = () => {
         turnBtn.style.justifySelf = "end";
         return;
       }
+
       if (Controller.player2.status) {
         el.textContent = Controller.player2.sign;
         Controller.player2.status = false;
+        checkedGameboardArr.push(el);
         if (checkResult(Gameboard)) {
+          restartGame();
+          return;
+        } else if (
+          Gameboard.gameboardArr.length === checkedGameboardArr.length
+        ) {
+          turnBtn.style.justifySelf = "center";
+          turnBtn.textContent = "It's a tie";
           restartGame();
           return;
         } else {
@@ -119,28 +148,42 @@ const displayContent = () => {
     const restartBtn = document.querySelector(".restart-btn");
     const gameInfoDiv = document.querySelector(".game-info");
     const playerSigns = document.querySelectorAll(".player-sign");
-    turnBtn.textContent = "WIN";
+
+    if (turnBtn.textContent === "turn" || turnBtn.textContent === "TURN")
+      turnBtn.textContent = "WIN";
+
     restartBtn.classList.remove("hidden");
     gameInfoDiv.style.width = "60%";
 
     restartBtn.addEventListener("click", function () {
+      restartBtn.classList.add("hidden");
+      checkedGameboardArr = [];
       turnBtn.textContent = "TURN";
       Controller.player1.sign = Controller.player2.sign = "";
-      Gameboard.gameboardArr.forEach((el) => {
-        el.textContent = "";
-      });
       controllerDiv.classList.remove("hidden");
       gameboardCont.classList.add("hidden");
+
+      Gameboard.gameboardArr.forEach((el) => {
+        el.textContent = "";
+        if (el.classList.contains("winner-div"))
+          el.classList.remove("winner-div");
+      });
 
       playerSigns.forEach((el) => {
         el.remove();
       });
 
-      chosenBtns.forEach((el) => {
-        el.classList.remove("chosen");
+      selectBtns.forEach((el) => {
+        el.classList.remove("selected");
       });
     });
   };
+};
+
+const changeBackground = (arr) => {
+  arr.forEach((el) => {
+    el.classList.add("winner-div");
+  });
 };
 
 const checkResult = (arr) => {
@@ -148,42 +191,84 @@ const checkResult = (arr) => {
     arr.gameboardArr[0].textContent !== "" &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[2].textContent &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[1].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[0],
+      arr.gameboardArr[1],
+      arr.gameboardArr[2],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[0].textContent !== "" &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[3].textContent &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[6].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[0],
+      arr.gameboardArr[3],
+      arr.gameboardArr[6],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[0].textContent !== "" &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[4].textContent &&
     arr.gameboardArr[0].textContent === arr.gameboardArr[8].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[0],
+      arr.gameboardArr[4],
+      arr.gameboardArr[8],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[1].textContent !== "" &&
     arr.gameboardArr[1].textContent === arr.gameboardArr[4].textContent &&
     arr.gameboardArr[1].textContent === arr.gameboardArr[7].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[1],
+      arr.gameboardArr[4],
+      arr.gameboardArr[7],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[2].textContent !== "" &&
     arr.gameboardArr[2].textContent === arr.gameboardArr[5].textContent &&
     arr.gameboardArr[2].textContent === arr.gameboardArr[8].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[2],
+      arr.gameboardArr[5],
+      arr.gameboardArr[8],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[2].textContent !== "" &&
     arr.gameboardArr[2].textContent === arr.gameboardArr[4].textContent &&
     arr.gameboardArr[2].textContent === arr.gameboardArr[6].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[2],
+      arr.gameboardArr[4],
+      arr.gameboardArr[6],
+    ]);
     return true;
+  }
   if (
     arr.gameboardArr[3].textContent !== "" &&
     arr.gameboardArr[3].textContent === arr.gameboardArr[4].textContent &&
     arr.gameboardArr[3].textContent === arr.gameboardArr[5].textContent
-  )
+  ) {
+    changeBackground([
+      arr.gameboardArr[3],
+      arr.gameboardArr[4],
+      arr.gameboardArr[5],
+    ]);
     return true;
+  }
 };
